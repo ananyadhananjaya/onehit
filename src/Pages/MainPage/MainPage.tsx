e7eacb01cab554743be610cab6330bc33392d522
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-08-16 18:48:41 +0530

    feat: Link Cards Skeleton

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { auth, storage } from '../../api/firebase.config'
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import LinkCardComponent from '../../components/LinkCardComponent'
 
 const MainPage = () => {
   const navigate = useNavigate()
@@ -96,8 +97,8 @@ const MainPage = () => {
   }
 
   return (
-    <div>
-      <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-2 p-2 items-center bg-blue-50">
+      {/* <div className="flex flex-col gap-4">
         <div>
           <div className="my-3">Username</div>
           <div>
@@ -130,8 +131,13 @@ const MainPage = () => {
       </div>
       <div>
         <button
-          className="bg-blue-100 px-8 rounded-xl p-2 shadow-2xl text-gray-600 font-medium hover:shadow-soft-ui hover:bg-blue-50"
+          className={`${
+            file && file.name && file.name.length > 0
+              ? 'bg-blue-100 px-8 rounded-xl p-2 shadow-2xl text-gray-600 font-medium hover:shadow-soft-ui hover:bg-blue-50'
+              : 'bg-blue-50 px-8 rounded-xl p-2 shadow-2xl text-gray-600 font-medium'
+          }`}
           onClick={() => handleUpload(file)}
+          disabled={!(file && file.name && file.name.length > 0)}
         >
           Upload
         </button>
@@ -140,7 +146,42 @@ const MainPage = () => {
         <img src={imgUrl} alt="avatar" className="rounded-full w-48 h-48" />
       </div>
       <div>Welcome {email}</div>
-      <div>Welcome {displayname}</div>
+      <div>Welcome {displayname}</div> */}
+      <div className="w-11/12 md:h-80 h-max-content p-2 flex flex-col bg-slate-50 rounded-xl ">
+        <div className="w-full h-40 bg-slate-300 rounded-tr-xl rounded-tl-xl"></div>
+        <div className="flex rounded-full w-36 h-36 bg-slate-50 relative bottom-12 left-2">
+          <img
+            src={imgUrl}
+            alt="Profile Picture"
+            className="w-32 h-32 m-auto bg-black rounded-full"
+          />
+        </div>
+        <div className="flex sm:justify-between flex-col sm:flex-row justify-center  px-2">
+          <div>
+            <div className="font-medium text-lg text-slate-600">
+              {displayname}
+            </div>
+            <div className="text-slate-500">Software Engineer</div>
+            <div className="text-slate-500">Los Angeles, California</div>
+          </div>
+          <div>
+            <div className="text-sm font-bold text-slate-500">{email}</div>
+            <div className="text-sm font-bold text-slate-500">
+              +01 7698 98897
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-9/12 pt-10 flex flex-wrap justify-center gap-y-8 gap-x-4">
+        <LinkCardComponent />
+        <LinkCardComponent />
+
+        <LinkCardComponent />
+        <LinkCardComponent />
+
+        <LinkCardComponent />
+        <LinkCardComponent />
+      </div>
     </div>
   )
 }
---
+++ src/components/LinkCardComponent/LinkCardComponent.tsx
@@ -0,0 +1,9 @@
+const LinkCardComponent = () => {
+  return (
+    <div className="w-64 h-24 bg-blue-50 px-4 shadow-soft-ui-v2 rounded-xl">
+      test
+    </div>
+  )
+}
+
+export default LinkCardComponent
