c2fe4fd7689172eca159d9a8885a474603409fbb
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-08-16 18:47:43 +0530

    feat: Footer Component

---
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -0,0 +1,10 @@
+const FooterComponent = () => {
+  return (
+    <div className="bottom-2 absolute text-slate-500 text-sm">
+      Released under the MIT License. Copyright © {new Date().getFullYear()}{' '}
+      Ananya Dhananjaya
+    </div>
+  )
+}
+
+export default FooterComponent
