322202ba6e0408be9c0bb5ed164869e48ae023d7
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-08-16 18:48:14 +0530

    chore: footer and bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import FooterComponent from './components/FooterComponent'
 import LoginPage from './Pages/LoginPage'
 import MainPage from './Pages/MainPage'
 import PageNotFound from './Pages/PageNotFound'
@@ -6,13 +7,18 @@ import SignUpPage from './Pages/SignUpPage'
 
 function App() {
   return (
-    <div className="h-screen bg-blue-50">
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/onehit" element={<MainPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+    <div className="bg-blue-50 min-h-screen relative">
+      <div className="">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/onehit" element={<MainPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+      <div className="flex justify-center">
+        <FooterComponent />
+      </div>
     </div>
   )
 }
