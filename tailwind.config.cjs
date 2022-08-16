1b21bd348385230ddde3162475d8c0bb03a0522e
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-08-16 18:47:56 +0530

    chore: soft ui v2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,9 @@ module.exports = {
     extend: {
       boxShadow: {
         'soft-ui':
-          '-10px -10px 20px rgb(255, 255, 255,1), 10px 10px 20px rgb(172, 172, 179, 0.25)'
+          '-10px -10px 20px rgb(255, 255, 255,1), 10px 10px 20px rgb(172, 172, 179, 0.25)',
+        'soft-ui-v2':
+          '-5px -5px 30px rgb(255, 255, 255,1), 5px 5px 30px rgb(172, 172, 179, 0.25)'
       }
     }
   },
