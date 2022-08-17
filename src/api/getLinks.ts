2724ed2cf8b8d2072aaf9cceba963cd04f41814b
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-08-17 15:53:39 +0530

    fix: get api result

--- src/api/getLinks.ts
+++ src/api/getLinks.ts
@@ -4,8 +4,11 @@ import { addDoc, collection } from 'firebase/firestore'
 
 export default async (userId: any) => {
   const querySnapshot = await getDocs(collection(getFirestore(app), userId))
+  let dataArray: any[] = []
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
     console.log(doc.id, ' => ', doc.data())
+    dataArray.push(doc.data())
   })
+  return dataArray
 }
