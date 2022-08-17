b6fdfab1f7a963d2166d2c3d49e0b12bae3a3a6b
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-08-17 15:53:50 +0530

    feat: Edit and Delete

--- src/components/LinkCardComponent/LinkCardComponent.tsx
+++ src/components/LinkCardComponent/LinkCardComponent.tsx
@@ -1,7 +1,58 @@
-const LinkCardComponent = () => {
+import { FaRegEdit } from 'react-icons/fa'
+import { AiOutlineDelete } from 'react-icons/ai'
+import { useState } from 'react'
+import createLink from '../../api/createLink'
+import deleteLink from '../../api/deleteLink'
+
+interface Props {
+  link: string
+  linkType: string
+  userId: string | undefined
+}
+
+const LinkCardComponent = (props: Props) => {
+  const { link, linkType, userId } = props
+  const [editFlag, setEditFlag] = useState<boolean>(false)
+  const [newLink, setNewLink] = useState<string>(link)
+
+  const handleLinkUpdate = () => {
+    setEditFlag(!editFlag)
+    createLink(userId, linkType, newLink)
+  }
+
+  const handleDelete = () => {
+    deleteLink(userId, linkType)
+  }
+
   return (
-    <div className="w-64 h-24 bg-blue-50 px-4 shadow-soft-ui-v2 rounded-xl hover:scale-110">
-      test
+    <div className="bg-zinc-50 p-2 px-4 text-sm flex gap-4 items-center whitespace-normal shadow-soft-ui-v2 rounded-lg hover:scale-110">
+      <div className="flex flex-col gap-4">
+        <div>Type: {linkType}</div>
+        {editFlag ? (
+          <div>
+            <input
+              type="text"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              className=" p-2 outine-none rounded-xl bg-slate-50"
+              onBlur={handleLinkUpdate}
+            />
+          </div>
+        ) : (
+          <div>Link -{newLink} </div>
+        )}
+      </div>
+      <div className="text-gray-400 flex flex-col gap-4 hover:cursor-pointer">
+        <div className="hover:text-gray-800" onClick={handleDelete}>
+          <AiOutlineDelete size={20} />
+        </div>
+        <div
+          className="hover:text-gray-800"
+          onClick={() => setEditFlag(!editFlag)}
+        >
+          <FaRegEdit size={18} />
+        </div>
+      </div>
     </div>
   )
 }
