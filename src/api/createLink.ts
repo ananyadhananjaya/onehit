d80849b249ce00185aad4c623ab7f387a842f4f0
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-08-17 11:06:56 +0530

    feat: Create Link API

---
+++ src/api/createLink.ts
@@ -0,0 +1,15 @@
+import { db } from './firebase.config'
+import { addDoc, setDoc, collection, doc } from 'firebase/firestore/lite'
+
+export default async (userId: any, linkType: string, link: string) => {
+  try {
+    let res = await setDoc(doc(db, userId, 'instagram'), {
+      link: link,
+      linkType: linkType
+    })
+    return 0
+  } catch (e) {
+    console.log('Error adding doc', e)
+    return 0
+  }
+}
